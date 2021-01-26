3287b4d5fb297e34cc00cecc04cfd07f5bfca6d7
carlylongo16 <carlongo106@gmail.com>
2021-01-26 18:47:35 -0500

    commented out all the unneccesary code

--- testing.py
+++ testing.py
@@ -15,23 +15,18 @@ def on_publish(client,userdata,result):             #create function for callbac
     pass
 
 client = mqtt.Client("AED_rasp_pi")
-client.on_connect = on_connect
-client.on_publish = on_publish  
+#client.on_connect = on_connect
+#client.on_publish = on_publish  
 #client.on_message = on_message
 #client.loop_start()
 #client.connect("192.168.43.81", 1883, 60)
-print("Connecting to broker ","maqiatto.com")
+#print("Connecting to broker ","maqiatto.com")
 client.username_pw_set("tuf53905@temple.edu", password="GMPQTtw7")
 client.connect("maqiatto.com", 1883, 60 )
 client.publish("tuf53905@temple.edu/AEDAutoDelivery","on")                   #publish
 
 
-client.loop_start()
+#client.loop_start()
 
-
-while not client.connected_flag: #wait in loop
-    print("In wait loop")
-    time.sleep(1)
-print("in Main Loop")
-client.loop_stop()    #Stop loop 
-client.disconnect() # disconnect
+#client.loop_stop()    #Stop loop 
+#client.disconnect() # disconnect
