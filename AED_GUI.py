d9d447354eb42b027c96883bd5f4b9c47f2e49b6
carlylongo16 <carlongo106@gmail.com>
2021-02-04 14:22:49 -0500

    AED_GUI is now object oriented, making it easier to transfer information from one window to the other

--- AED_GUI.py
+++ AED_GUI.py
@@ -12,83 +12,62 @@ import signal
 from idle import Idle
 from Data_class import *
 
-
-#define root window
-root = Tk()
-root.title("Automated AED Delivery System")
-#add an icon
-#root.iconbitmap("@/home/pi/Automated_AED_System/temple_logo.ico")
-img = PhotoImage(file='temple_logo.png')
-root.tk.call('wm', 'iconphoto', root._w, img)
-
-#state global variable delcaration 
-#used in main window label to show active status
-global status
-status=StringVar()
-status.set("Not Initialize")
-
-
-#define button functions
-#def idle_call():
-	#change status of the label in root
-#	status.set("Idle State")
-#	status_label.config(text=status.get())
-#	idle_state()
-#	return
-
-#called when test button is hit in root
-def test_call():
-	#change status of the label in root
-	status.set("Testing State")
-	status_label.config(text=status.get())
-	###calls the MQTT as of right now
-	testing_called()
-	return 
-
-
-#called when initialze button is called in root window
-def init_call():
-    status.set("Initializing State")
-    status_label.config(text=status.get())
-    init_data = Initialize()
-    
-
-
-def idle_call():
-    idle_window = Idle(file_path)
-    while idle_window.idle_stop != 0:
-        idle_window.wait_for_signal()
+class AED_GUI(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('1000x400')
+        self.title("Automated AED Delivery System")
+        self.status=StringVar()
+        self.status="Not Initialize"
         
-    del idle_window
+        #add labels 
+        self.welcome_label = Label(self, text="Welcome to the Automated AED Delivery System", font=("Arial", 25), width=50, borderwidth=5)
+        self.status_label = Label(self,text=self.status, font=("Arial", 16))
+        self.welcome_label.grid(row=0,column=0, columnspan=3)
+        self.status_label.grid(row=1,column=0,columnspan = 3, pady = 10)
+        
+        # #define buttons
+        self.idle_button = Button(self, padx = 50, pady = 50, text="Idle State", command=self.idle_call)
+        self.test_button = Button(self, padx = 50, pady = 50, text="Testing State", command=self.test_call)
+        self.initialize_button = Button(self, padx = 50, pady = 50, text="Initializing State", command=self.init_call)
+
+        self.idle_button.grid(row=2,column=0, padx = (25,0), pady=(0,100))
+        self.test_button.grid(row=2,column=2, padx = (0,25), pady=(0,100))
+        self.initialize_button.grid(row=2,column=1, padx = (25,25), pady=(0,100))
+        
+        self.init_data = None
+        
+
+    # #called when test button is hit in root
+    def test_call(self):
+        #change status of the label in root
+        self.status = "Testing State"
+        self.status_label.config(text=self.status)
+        ###calls the MQTT as of right now
+        testing_called()
+        return 
+
+
+    #called when initialze button is called in root window
+    def init_call(self):
+        self.status = "Initializing State"
+        self.status_label.config(text=self.status)
+        self.init_data = Initialize()
     
-    return
-    
 
-#add labels 
-welcome_label = Label(root, text="Welcome to the Automated AED Delivery System", font=("Arial", 25), width=50, borderwidth=5)
-status_label = Label(root,text=status.get(), font=("Arial", 16))
-welcome_label.grid(row=0,column=0, columnspan=3)
-status_label.grid(row=1,column=0,columnspan = 3, pady = 10)
-	
-#define buttons
-idle_button = Button(root, padx = 50, pady = 50, text="Idle State", command=idle_call)
-test_button = Button(root, padx = 50, pady = 50, text="Testing State", command=test_call)
-initialize_button = Button(root, padx = 50, pady = 50, text="Initializing State", command=init_call)
 
-idle_button.grid(row=2,column=0, padx = (25,0), pady=(0,100))
-test_button.grid(row=2,column=2, padx = (0,25), pady=(0,100))
-initialize_button.grid(row=2,column=1, padx = (25,25), pady=(0,100))
+    def idle_call(self):
+        self.status = "Idle State"
+        self.status_label.config(text=self.status)
+        idle_window = Idle(self.init_data.data_class.file_name)
+        while idle_window.idle_stop != 0:
+            idle_window.wait_for_signal()
+            
+        del idle_window
+        
+        return
 
-# ~ def openwindow():
-	# ~ #global variables are needed within the top level windows
-	# ~ top = Toplevel()
-	# ~ top.title("second window")
-	# ~ #add an icon
-	# ~ top.iconbitmap("C:/Users/User1/Documents/SENIOR DESIGN/Automated_AED_Delivery/temple_logo.ico")
-	# ~ lbl = Label(top, text="Hello World").pack()
-	# ~ btn2 = Button(top, text="close window", command=top.destroy).pack()
-
-# ~ btn = Button(root, text="open second window", command=openwindow).pack()
-
+root = AED_GUI()
 
 root.mainloop()
+
